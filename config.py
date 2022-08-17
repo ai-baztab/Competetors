0674b4567f8ebee1a8c6363d172bb210cb86a548
s.yaghoobi_pr <data.ai.baztab@gmail.com>
2022-08-17 12:17:51 +0430

    complete some config names and lists

--- config.py
+++ config.py
@@ -1,8 +1,12 @@
 url = 'https://www.digikala.com/search/{s}'
-
+symbol_to_search = 'category-milk/'
 products = {
-    'milk' : 'category-milk/',
-    'doogh': ''
+    'milk': 'category-milk/',
+    'doogh': 'category-doogh/'
+}
+digi_btn = {
+    'visited': 'مرتبط‌ترین',
+    'sold_out':'پرفروش‌ترین'
 }
 most_visited_btn = '/html/body/div[1]/div[1]/div[3]/div[3]/div[1]/div[2]/section[1]/div[1]/div/div[2]/div[2]'
 most_sold_btn = '/html/body/div[1]/div[1]/div[3]/div[3]/div[2]/section[1]/div[1]/div/div[2]/div[4]'
